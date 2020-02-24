b6d86941088cc9fd01d97804823c15ebd3091c54
Anthony Villano <anthony.villano@ucdenver.edu>
2020-02-24 15:03:06 -0600

    [ANV] switch over travis testing to the analysis_documentation folder ONLY by modifying the top-level .travis.yml file

---
+++ analysis_documentation/test_local.py
@@ -0,0 +1,24 @@
+import subprocess
+import tempfile
+import papermill as pm
+import os.path as path
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--ExecutePreprocessor.kernel_name=python3",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+def _exec_papermill(input_nb, args):
+    output_nb = path.join('test',input_nb)
+    pm.execute_notebook(input_nb, output_nb, parameters=args)
+
+
+def test():
+    print('Testing Jupyter notebooks...')
+    _exec_notebook('nrFano_documentation.ipynb')
+    #_exec_papermill('edelweiss_res.ipynb', None)
+    #_exec_papermill('edelweiss_C_systematicErrors_allParameters.ipynb', {'Test': True})
