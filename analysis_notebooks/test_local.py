adb4675e38d1a1958abbeace2c12d849531a7207
Amy Roberts <amy.roberts@ucdenver.edu>
2020-04-07 12:04:49 -0600

    see if bin_centering_correction notebook causes issues

--- analysis_notebooks/test_local.py
+++ analysis_notebooks/test_local.py
@@ -31,6 +31,6 @@ def test():
     #_exec_notebook('ms_correction.ipynb')
     #_exec_notebook('yield_width_compare.ipynb')
     #_exec_notebook('fitting_errors.ipynb')
-    #_exec_notebook('bin_centering_correction.ipynb')
-    _exec_papermill('edelweiss_res.ipynb', None)
+    _exec_notebook('bin_centering_correction.ipynb')
+    #_exec_papermill('edelweiss_res.ipynb', None) # causes problem
     #_exec_papermill('edelweiss_C_systematicErrors_allParameters.ipynb', {'Test': True})
