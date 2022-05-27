33cb9e7159d4bf2181662361ae39a12303722875
Anthony Villano <anthony.villano@ucdenver.edu>
2022-05-27 17:03:10 -0600

    [ANV] adding a fano extracting library

---
+++ python/fano_extract.py
@@ -0,0 +1,25 @@
+import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import h5py
+#warnings.resetwarnings()
+from scipy.integrate import quad
+import resfuncRead as rfr
+import scipy.optimize as so
+import prob_dist as pd
+import os
+import scipy.interpolate as inter
+
+def get_sigvec(Evec,csample,cpsample):
+
+    #check Evec to be an numpy vector of appropriate shape?
+
+    #check csample and cpsample for correct size
+
+    #construct rhs
+    #print(csample[3])
+    var = lambda E: pd.series_NRQ_var_corr1(E,0.0,csample[3]*4.0,csample[0],(1/18.0),csample[4],csample[5])
+    varv = np.vectorize(var)
+    print(varv(Evec))
+
+    return
