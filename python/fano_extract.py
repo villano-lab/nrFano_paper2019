634ddf13f4b5b401ae39efc5cc8cec46b300af1a
Anthony Villano <anthony.villano@ucdenver.edu>
2022-05-27 17:54:06 -0600

    [ANV] updated fano_extract

--- python/fano_extract.py
+++ python/fano_extract.py
@@ -9,6 +9,7 @@ import scipy.optimize as so
 import prob_dist as pd
 import os
 import scipy.interpolate as inter
+from scipy import optimize
 
 def get_sigvec(Evec,csample,cpsample):
 
@@ -30,3 +31,21 @@ def get_sigvec(Evec,csample,cpsample):
     retv = np.sqrt(varv(Evec)-(cpsample[0]+Evec*cpsample[1])**2+(csample[1]+Evec*csample[2])**2)
 
     return retv
+
+def get_root_from_sigvec(Evec,csample,cpsample):
+
+    #get the vector of sigmas
+    sigvec = get_sigvec(Evec,csample,cpsample)
+
+    #loop through Eveca
+    Fvec = np.zeros(np.shape(Evec))
+    for Er,i in enumerate(Evec):
+
+      #set up the fano function
+      fano = lambda F: np.sqrt(pd.series_NRQ_var_corr1(Er,F,csample[3]*4.0,csample[0],(1/18.0),csample[4],csample[5])) - sigvec[i]
+
+      print(pd.series_NRQ_var_corr1(Er,10,csample[3]*4.0,csample[0],(1/18.0),csample[4],csample[5]))
+      #get the root
+      #Fvec[i] = optimize.root_scalar(fano, bracket=[0.1, 200], method='brentq').root 
+
+    return Fvec
